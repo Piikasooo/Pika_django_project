5fe49a588fbee3cf0cb4d3cdbb8347c849027016
Artem <artger@ukr.net>
2019-07-13 14:40:10 +0300

    Add input in homepage.html, creat new test in red_test

--- PROJECT_TESTS/red_test.py
+++ PROJECT_TESTS/red_test.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 import pytest
+import time
 
 
 @pytest.fixture
@@ -10,5 +11,12 @@ def browser():
 
 
 def test_title(browser):
+    time.sleep(3)
     browser.get('http://127.0.0.1:8000/')
+    header_text = browser.find_element_by_tag_name('h1').text
+    input_box = browser.find_element_by_id("test_placeholder")
+    input_box.send_keys("Hello world")
+    time.sleep(3)
+
+    assert header_text == "Pikasooo"
     assert browser.title == "PROJECT"
